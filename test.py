6d871f5f4c1c74a4b42b9898696dcbfd2dae2ba0
GlowAnder <moyogloria419@gmail.com>
2025-03-10 13:24:20 +0100

    Create test.py
    this information gives an introduction of me

---
+++ test.py
@@ -0,0 +1,6 @@
+name='Peace Gloria'
+age=16
+height=1.4
+gender='female'
+natioanality='Malawian'
+print(f'my name is {name},am {age} years old and am {height}metres tall ,am {gender} and am {nationality}')
